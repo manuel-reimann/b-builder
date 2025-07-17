57cc553e471071cc1e6fe91766d6e9dff1ce8d44
Manuel Reimann <91629388+manuel-reimann@users.noreply.github.com>
2025-07-17 23:42:45 +0200

    added debugging to vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+import { defineConfig, loadEnv } from "vite";
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  console.log("Loaded ENV:", env); // optional zur Prüfung
+
+  return {
+    define: {
+      "process.env": env,
+    },
+    // deine bestehende config hier
+  };
+});
