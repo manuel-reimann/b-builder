e1733add58f35cc68a97ef12674327ca97effb60
Manuel Reimann <91629388+manuel-reimann@users.noreply.github.com>
2025-07-18 00:56:24 +0200

    what is going on wtf

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,15 @@ import "./index.css";
 import App from "./App.tsx";
 import { DndContext } from "@dnd-kit/core";
 
+console.log("MAIN ENTRY REACHED");
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <DndContext
       onDragEnd={(event) => {
         const { over, active } = event;
         if (over?.id === "canvas") {
-          console.log(`Dropped '${active.id}' into canvas`);
-          // TODO: Element im Canvas anzeigen
+          console.log(`Dropped '${active.id}' into canvas`); // shows the id of the dragged item
         }
       }}
     >
