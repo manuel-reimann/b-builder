52fe5f6ad92cb87c323c7bfbe062b3e8424e3683
Manuel Reimann <91629388+manuel-reimann@users.noreply.github.com>
2025-07-24 23:27:52 +0200

    wrong path

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -76,7 +76,7 @@ export default function Canvas({
   const resolvedBackgroundSrc: string | undefined = backgroundSrc
     ? backgroundSrc.startsWith("http") || backgroundSrc.startsWith("/")
       ? backgroundSrc
-      : `/img/backgrounds/${backgroundSrc}.webp`
+      : `/img/bgs/${backgroundSrc}.webp`
     : undefined;
   // Reference to the Konva Stage component
   const stageRef = useRef<any>(null);
